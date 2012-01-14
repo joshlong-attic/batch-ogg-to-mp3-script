ff555566b55bbe9483c5717eabdcd4e7b0dd9d86
Josh Long <josh.long@springsource.com>
2012-01-14 13:32:04 -0800

    adding shell script.

--- src/main/resources/run_linux.sh
+++ src/main/resources/run_linux.sh
@@ -1,3 +1,3 @@
-export MUSIC_HOME=/Volumes/music
-export FFMPEG_CMD = /usr/local/bin/ffmpeg
+export MUSIC_HOME="/home/jlong/Desktop/mp3s"
+export FFMPEG_CMD="/usr/local/bin/ffmpeg"
 java -jar oggtomp3.jar
